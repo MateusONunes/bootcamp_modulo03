ca0c8bb81f1a6575eb9640b4740cc3b1a29cb385
Mateus Nunes <mateus.2006@gmail.com>
2020-02-01 20:02:31 -0300

    Finalização do Módulo 3

---
+++ data.js
@@ -0,0 +1,42 @@
+module.exports = [
+
+    {
+        id:"KZogBElzzb0",
+        title:"Devotos do divino",
+        duration:"5 Min.",
+        price:"Free",
+        featured: true
+    },
+    {
+        id:"hZv3-Bp-Y2Q",
+        title:"Mãe Natureza",
+        duration:"4 Min.",
+        price:"Free"
+    },
+    {
+        id:"KZogBElzzb0",
+        title:"Mateus cantando Devotos do divino",
+        duration:"5 Min.",
+        price:"Free",
+        featured: true
+    },
+    {
+        id:"KZogBElzzb0",
+        title:"Mateus cantando Devotos do divino",
+        duration:"5 Min.",
+        price:"Free"
+    },
+    {
+        id:"KZogBElzzb0",
+        title:"Mateus cantando Devotos do divino",
+        duration:"5 Min.",
+        price:"Free"
+    },
+    {
+        id:"KZogBElzzb0",
+        title:"Mateus cantando Devotos do divino",
+        duration:"5 Min.",
+        price:"Free"
+    },
+
+]
